25e51a7cd69c7296e9b39d7b1c972aeafe8c4363
unknown <oleks.dudynets2004@gmail.com>
2021-03-15 14:04:08 +0200

    v.1.0.0

---
+++ judges/app.js
@@ -0,0 +1,426 @@
+/*
+    Debate Reviews Tool v0.9.1
+    Copyright 2021 Oleksandr Dudynets
+
+    Permission is hereby granted, free of charge,
+    to any person obtaining a copy of this software
+    and associated documentation files (the "Software"),
+    to deal in the Software without restriction,
+    including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit
+    persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice
+    shall be included in all copies or substantial
+    portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY
+    OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+    LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+    EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+    FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+    AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+    Date: 2021-02-26
+*/
+
+let RECIPIENTS = {};
+let SPREADSHEET_ID = '';
+
+let isConnected = false;
+onlineListener();
+
+firebase.initializeApp(FIREBASE_CONFIG);
+let database = firebase.database();
+
+let authorizeButton = document.getElementById('authorize');
+let signoutButton = document.getElementById('signout');
+
+let sendButton = document.getElementById('send');
+let downloadButton = document.getElementById('download');
+let clearTableButton = document.getElementById('clearTable');
+
+let userImageUrl, userName, userEmail;
+let userImage = document.getElementById('userImage');
+let userNameText = document.getElementById('name');
+let userEmailText = document.getElementById('email');
+let userInfoDiv = document.getElementById('userInfoDiv');
+
+let changeIDButton = document.getElementById('changeID');
+let showRecipientsButton = document.getElementById('showRecipients');
+let changeRecipientsButton = document.getElementById('changeRecipients');
+let getBotUpdatesButton = document.getElementById('getBotUpdates');
+let loader = document.getElementById('loader');
+let section = document.getElementById('section');
+let loadingText = document.getElementById('loadingText');
+
+let popupBoxDiv = document.getElementById('popupBox');
+let popupHeading = document.getElementById('popupHeading');
+let popupContent = document.getElementById('popupPre');
+
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+        loadingText.style.display = 'none';
+        isConnected = true;
+    });
+}
+
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        let profile = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile();
+        userName = profile.getName();
+        userImageUrl = profile.getImageUrl();
+        userEmail = profile.getEmail();
+        userImage.src = userImageUrl;
+        userNameText.innerHTML = userName;
+        userEmailText.innerHTML = `<a href="mailto:${userEmail}">${userEmail}</a>`;
+
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'inline';
+        sendButton.style.display = 'inline';
+        downloadButton.style.display = 'inline';
+        clearTableButton.style.display = 'inline';
+        userInfoDiv.style.display = 'table';
+        changeIDButton.style.display = 'block';
+        showRecipientsButton.style.display = 'block';
+        changeRecipientsButton.style.display = 'block';
+        getBotUpdatesButton.style.display = 'block';
+        loader.style.display = 'none';
+        section.style.display = 'inline';
+
+        database.ref('judges/recipients').get().then(function(snapshot) {
+            alertify.success("Recipients database connected");
+            RECIPIENTS = snapshot.val();
+        }).catch(function(error) {
+            alertify.error("Database error");
+            console.error(error);
+        });
+
+        database.ref('judges/spreadsheet_id').get().then(function(snapshot) {
+            alertify.success("Spreadsheet ID database connected");
+            SPREADSHEET_ID = snapshot.val();
+        }).catch(function(error) {
+            alertify.error("Database error");
+            console.error(error);
+        });
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+        sendButton.style.display = 'none';
+        downloadButton.style.display = 'none';
+        clearTableButton.style.display = 'none';
+        userInfoDiv.style.display = 'none';
+        changeIDButton.style.display = 'none';
+        showRecipientsButton.style.display = 'none';
+        changeRecipientsButton.style.display = 'none';
+        getBotUpdatesButton.style.display = 'none';
+    }
+}
+
+function handleAuthClick(event) {
+    loader.style.display = 'flex';
+    section.style.display = 'none';
+    gapi.auth2.getAuthInstance().signIn().catch(() => {
+        alertify.error("Sign In error")
+    })
+}
+
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut().catch(() => {
+        alertify.error("Sign Out error")
+    });
+}
+
+function cancelLogIn() {
+    loader.style.display = 'none';
+    section.style.display = 'inline';
+    location.reload();
+    return false;
+}
+
+async function getSortedValues() {
+    let response = await gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: SPREADSHEET_ID,
+        range: JUDGES_RANGES,
+    });
+
+    let receivedValues = response.result.values;
+        let sortedValues = {};
+
+        if (receivedValues) {
+            for (row in receivedValues) {
+                if (!sortedValues[receivedValues[row][2]]) {
+                    sortedValues[receivedValues[row][2]] = [];
+                }
+
+                let obj = {
+                    timestamp: receivedValues[row][0], 
+                    name: receivedValues[row][1], 
+                    position: receivedValues[row][3], 
+                    mark1: receivedValues[row][4], 
+                    mark2: receivedValues[row][5], 
+                    mark3: receivedValues[row][6], 
+                    message: receivedValues[row][7], 
+                }
+
+                sortedValues[receivedValues[row][2]].push(obj);
+            }
+        } else {
+            alertify.error('The table is empty');
+        }
+
+        return sortedValues;
+}
+
+async function send() {
+    try {
+        let fullText = '';
+
+        let values = await getSortedValues()
+            .catch(() => {
+                throw new Error('Error retrieving data')
+            });
+
+        for (recipient in values) {
+            let text = formatMessage(values, recipient, true, true);
+
+            for (recipientItem in RECIPIENTS) {
+                if (recipientItem == recipient || RECIPIENTS[recipientItem]["isAdmin"]) {
+                    let url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${RECIPIENTS[recipientItem]["tID"]}&text=` + encodeURI(text) + `&parse_mode=markdown`;
+                    try {
+                        const request = await fetch(url);
+                        const data = await request.json();
+                        if (data.ok) {
+                            alertify.success("Sent successfully");
+                            let report = `■ Sender: ${data.result.from.first_name ? data.result.from.first_name : ""}${data.result.from.last_name ? " " + data.result.from.last_name : ""} (${data.result.from.id ? data.result.from.id : ""}${data.result.from.username ? ":" + data.result.from.username : ""})\n\n■ Recipient: ${data.result.chat.first_name ? data.result.chat.first_name : ""}${data.result.chat.last_name ? " " + data.result.chat.last_name : ""} (${data.result.chat.id ? data.result.chat.id : ""}${data.result.chat.username ? ":" + data.result.chat.username : ""})\n\n■ Message:\n\n${data.result.text}\n\n***\n\n`;
+                            console.log(report);
+                            fullText = fullText.concat(report);
+                        } else {
+                            throw new Error('Error sending data');
+                        }
+                    } catch (e) {
+                        throw new Error('Error sending data');
+                    }
+                }
+            }
+        }
+    } catch (e) {
+        console.error(e);
+        alertify.error(e.message);
+    }
+}
+
+async function download() {
+    try {
+        let isDownloadNeeded = confirm("Do you want to download report?");
+        let fullText = '';
+
+        let values = await getSortedValues()
+            .catch(() => {
+                throw new Error('Error retrieving data')
+            });
+
+        for (recipient in values) {
+            let text = formatMessage(values, recipient, false, false);
+            fullText = fullText.concat(text + '\n<hr noshade color="black" size="1px">\n');
+            if (isDownloadNeeded) downloader(`${recipient}.txt`,text);
+        }
+
+        showPopup('Report:', fullText);
+    } catch (e) {
+        console.error(e);
+        alertify.error(e.message);
+    }
+}
+
+function clearTable() {
+    let submitDelete = confirm("Are you sure want to clear the table?");
+    if (submitDelete) {
+        try {
+            let request = gapi.client.sheets.spreadsheets.values.batchClear({
+                spreadsheetId: SPREADSHEET_ID
+            }, {
+                ranges: JUDGES_RANGES
+            });
+
+            request.then(function() {
+                alertify.success('Table has been cleaned');
+            }, function(reason) {
+                console.error('error: ' + reason.result.error.message);
+                alertify.error('Table cleaning error');
+            });
+        } catch (e) {
+            console.error(e);
+            alertify.error('Error cleaning table');
+        }
+            
+    }
+}
+
+function showPopup(heading, recipients) {
+    popupBoxDiv.style.display = 'block';
+    
+    popupHeading.innerHTML = heading;
+
+    if (typeof recipients == "object") {
+        popupContent.innerHTML = JSON.stringify(recipients, undefined, 4);
+    } else {
+        popupContent.innerHTML = recipients;
+    }
+
+}
+
+function closePopup() {
+    popupBoxDiv.style.display = 'none';
+
+}
+
+function changeRecipients() {
+    let newRecipients = prompt("Set recipients dataset (JSON):", JSON.stringify(RECIPIENTS));
+
+    if (newRecipients) {
+        try {
+            let parsedNewRecipients = JSON.parse(newRecipients);
+            showPopup('Recipients:', parsedNewRecipients);
+            RECIPIENTS = parsedNewRecipients;
+            database.ref('judges/recipients').set(RECIPIENTS);
+            alertify.success("Recipients changed");
+        } catch(e) {
+            alertify.error("JSON parse error")
+            console.log(e)
+        }
+    }
+}
+
+function changeSheetID() {
+    let newSpreadsheetID = prompt("Enter sheet ID:", SPREADSHEET_ID);  
+
+    if (newSpreadsheetID) {
+        showPopup('Spreadsheet ID:', newSpreadsheetID);
+        SPREADSHEET_ID = newSpreadsheetID;
+        database.ref('judges/spreadsheet_id').set(SPREADSHEET_ID);
+        alertify.success("Spreadsheet ID changed");
+    }
+}
+
+function getBotUpdates() {
+    let url = `https://api.telegram.org/bot${BOT_TOKEN}/getUpdates`;
+    let fullText = '';
+
+    loader.style.display = 'flex';
+    section.style.display = 'none';
+
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+        loader.style.display = 'none';
+        section.style.display = 'inline';
+        for (message in data.result) {
+            if (data.result[message].message) {
+                let messageItem = data.result[message].message;
+                let timestamp = new Date(messageItem.date * 1000);
+                let formattedMessage = `• Sender: ${messageItem.from.first_name ? messageItem.from.first_name : ""}${messageItem.from.last_name ? " " + messageItem.from.last_name : ""} (${messageItem.from.id ? messageItem.from.id : ""}${messageItem.from.username ? ':<a style="color: black;" href="https://t.me/'+ messageItem.from.username + '">' + messageItem.from.username + '</a>' : ""})\n• Date: ${formatDate(timestamp)}\n• Message: ${messageItem.text}\n\n<hr noshade color="black" size="1px">\n`;
+                fullText = fullText.concat(formattedMessage);
+            }
+        }
+        showPopup('Bot updates:', fullText);
+    });
+}
+
+function formatMessage(values, recipient, isBold, isStickers) {
+    let markdownChar = '';
+    if (isBold) {
+        markdownChar = '*';
+    }
+
+    let stickers = ['•', '•', '•', '•', '•', '•', '•', '■']
+    if (isStickers) {
+        stickers = ['👤', '⏰', '📢', '1️⃣', '2️⃣', '3️⃣', '✉️', '📋'];
+    }
+
+    let text = `${markdownChar}${stickers[7]} ЗВІТ ДЛЯ СУДДІ: ${recipient}${markdownChar}\n`;
+
+    for (review in values[recipient]) {
+        let reviewItem = values[recipient][review]
+
+        let messageText = '';
+        if (reviewItem.message) {
+            messageText = `\n${stickers[6]} ${markdownChar}Коментар:${markdownChar} ${reviewItem.message}`;
+        }
+ 
+        let reviewText = `\n${stickers[0]} ${markdownChar}Ім'я:${markdownChar} ${reviewItem.name}\n${stickers[1]} ${markdownChar}Дата:${markdownChar} ${reviewItem.timestamp}\n${stickers[2]} ${markdownChar}Позиція:${markdownChar} ${reviewItem.position}\n${stickers[3]} ${markdownChar}Оцінка 1:${markdownChar} ${reviewItem.mark1}\n${stickers[4]} ${markdownChar}Оцінка 2:${markdownChar} ${reviewItem.mark2}\n${stickers[5]} ${markdownChar}Оцінка 3:${markdownChar} ${reviewItem.mark3}${messageText}\n`;
+        text = text.concat(reviewText);
+    }
+
+    return text;
+}
+
+function downloader(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+function formatDate(date) {
+    var dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+  
+    var mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+  
+    var yy = date.getFullYear() % 100;
+    if (yy < 10) yy = '0' + yy;
+
+    let hh = date.getHours();
+    if (hh < 10) hh = '0' + hh;
+
+    let mnmn = date.getMinutes();
+    if (mnmn < 10) mnmn = '0' + mnmn;
+
+    let ss = date.getSeconds();
+    if (ss < 10) ss = '0' + ss;
+  
+    return hh + ':' + mnmn + ':' + ss + ' ' + dd + '.' + mm + '.' + yy;
+}
+
+function onlineListener() {
+    window.addEventListener('load', event => {
+        setTimeout(() => {
+            if (isConnected == false) {
+                loadingText.innerHTML = `<p>Loading took longer than expected. Maybe you're offline.<br><br><span onclick="location.reload();return false;">Try again?</span></p>`
+            }
+        }, 5000)
+    })
+
+    window.addEventListener('online', event => {
+        alertify.dismissAll();
+        alertify.notify('You are online again', 'success', 0);
+    })
+
+    window.addEventListener('offline', event => {
+        alertify.dismissAll();
+        alertify.notify('You are offline', 'error', 0);
+    })
+}
---
+++ settings.js
@@ -0,0 +1,25 @@
+// SPREADSHEETS RANGES
+let PARTICIPANTS_RANGES = [''];
+let JUDGES_RANGES = [''];
+
+// TELEGRAM BOT TOKEN
+let BOT_TOKEN = '';
+
+// GOOGLE API CONFIG
+let CLIENT_ID = '';
+let API_KEY = '';
+
+// GOOGLE API SCOPES
+let DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+let SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
+
+// GOOGLE FIREBASE CONFIG
+let FIREBASE_CONFIG = {
+    apiKey: API_KEY,
+    authDomain: "",
+    projectId: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
